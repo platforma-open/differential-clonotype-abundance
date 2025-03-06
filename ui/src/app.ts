5be32ffea197906cdbdfa56b3e4bf360138abbf2
Alex Davydov <alex-d@live.co.uk>
2025-03-06 09:04:05 +0100

    de block copied

---
+++ ui/src/app.ts
@@ -0,0 +1,13 @@
+import { model } from '@platforma-open/MiLaboratories Inc.differential-clonotype-abundance.model';
+import { defineApp } from '@platforma-sdk/ui-vue';
+import MainPage from './pages/MainPage.vue';
+
+export const sdkPlugin = defineApp(model, () => {
+  return {
+    routes: {
+      '/': () => MainPage,
+    },
+  };
+});
+
+export const useApp = sdkPlugin.useApp;

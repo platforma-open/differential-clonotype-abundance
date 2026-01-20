5be16909726d99db326d08adf0428fc2f52c4f46
Mariia Zueva <maryzueva@gmail.com>
2026-01-20 10:15:10 +0100

    Label migration

--- ui/src/app.ts
+++ ui/src/app.ts
@@ -2,6 +2,7 @@ import { model } from '@platforma-open/milaboratories.differential-clonotype-abu
 import { defineApp } from '@platforma-sdk/ui-vue';
 import MainPage from './pages/MainPage.vue';
 import GraphPage from './pages/GraphPage.vue';
+import { watch } from 'vue';
 
 export const sdkPlugin = defineApp(model, () => {
   return {
@@ -14,3 +15,12 @@ export const sdkPlugin = defineApp(model, () => {
 });
 
 export const useApp = sdkPlugin.useApp;
+
+// Make sure labels are initialized
+const unwatch = watch(sdkPlugin, ({ loaded }) => {
+  if (!loaded) return;
+  const app = useApp();
+  app.model.args.customBlockLabel ??= '';
+  app.model.args.defaultBlockLabel ??= 'Select Clonotype Definition';
+  unwatch();
+});
